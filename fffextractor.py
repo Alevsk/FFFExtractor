75fe88a7f797235e02612945ae3c92b8a44db161
Lenin Alevski Huerta Arias <alevsk.8772@gmail.com>
2015-09-25 17:12:54 -0500

    cursorParser function done

--- fffextractor.py
+++ fffextractor.py
@@ -5,6 +5,7 @@ import httplib
 import zlib
 import urllib2
 import sys
+import string
 import json
 
 class CONSTANTS:
@@ -33,7 +34,7 @@ class FFFExtractor:
 		if data is None:
 			self.path = '/search/189386589649/likers'
 		else:
-			self.path = '/ajax/pagelet/generic.php/BrowseScrollingSetPagelet?data=' + '%7B%22typeahead_sid%22%3A%22%22%2C%22tr%22%3Anull%2C%22topic_id%22%3Anull%2C%22em%22%3Afalse%2C%22mr%22%3Afalse%2C%22view%22%3A%22list%22%2C%22display_params%22%3A%5B%5D%2C%22logger_source%22%3A%22www_main%22%2C%22encoded_query%22%3A%22%7B%5C%22bqf%5C%22%3A%5C%22likers(189386589649)%5C%22%2C%5C%22vertical%5C%22%3A%5C%22none%5C%22%2C%5C%22post_search_vertical%5C%22%3Anull%2C%5C%22intent_data%5C%22%3Anull%2C%5C%22query_analysis%5C%22%3Anull%7D%22%2C%22trending_source%22%3Anull%2C%22has_top_pagelet%22%3Anull%2C%22ref_path%22%3A%22%2Fsearch%2F189386589649%2Flikers%22%2C%22tl_log%22%3Afalse%2C%22encoded_title%22%3A%22WyJQZW9wbGUrd2hvK2xpa2UrIix7InRleHQiOiJVbml2ZXJzaWRhZCtQYW5hbWVyaWNhbmEiLCJ1aWQiOjE4OTM4NjU4OTY0OSwidHlwZSI6InBhZ2UifV0%22%2C%22is_trending%22%3Afalse%2C%22page_number%22%3A'+data['page_number']+'%2C%22browse_location%22%3A%22%22%2C%22filter_ids%22%3A%7B%22520053742%22%3A520053742%2C%221066595962%22%3A1066595962%2C%221406468036%22%3A1406468036%2C%22100000891774285%22%3A100000891774285%7D%2C%22callsite%22%3A%22browse_ui%3Ainit_result_set%22%2C%22cursor%22%3A%22A'+data['code']+'%22%2C%22exclude_ids%22%3Anull%2C%22impression_id%22%3A%229d6ea2b2%22%2C%22ref%22%3A%22unknown%22%2C%22experience_type%22%3A%22grammar%22%2C%22story_id%22%3Anull%7D'
+			self.path = '/ajax/pagelet/generic.php/BrowseScrollingSetPagelet?data=' + '%7B%22typeahead_sid%22%3A%22%22%2C%22tr%22%3Anull%2C%22topic_id%22%3Anull%2C%22em%22%3Afalse%2C%22mr%22%3Afalse%2C%22view%22%3A%22list%22%2C%22display_params%22%3A%5B%5D%2C%22logger_source%22%3A%22www_main%22%2C%22encoded_query%22%3A%22%7B%5C%22bqf%5C%22%3A%5C%22likers(189386589649)%5C%22%2C%5C%22vertical%5C%22%3A%5C%22none%5C%22%2C%5C%22post_search_vertical%5C%22%3Anull%2C%5C%22intent_data%5C%22%3Anull%2C%5C%22query_analysis%5C%22%3Anull%7D%22%2C%22trending_source%22%3Anull%2C%22has_top_pagelet%22%3Anull%2C%22ref_path%22%3A%22%2Fsearch%2F189386589649%2Flikers%22%2C%22tl_log%22%3Afalse%2C%22encoded_title%22%3A%22WyJQZW9wbGUrd2hvK2xpa2UrIix7InRleHQiOiJVbml2ZXJzaWRhZCtQYW5hbWVyaWNhbmEiLCJ1aWQiOjE4OTM4NjU4OTY0OSwidHlwZSI6InBhZ2UifV0%22%2C%22is_trending%22%3Afalse%2C%22page_number%22%3A'+data['page_number']+'%2C%22browse_location%22%3A%22%22%2C%22filter_ids%22%3A%7B%22520053742%22%3A520053742%2C%221066595962%22%3A1066595962%2C%221406468036%22%3A1406468036%2C%22100000891774285%22%3A100000891774285%7D%2C%22callsite%22%3A%22browse_ui%3Ainit_result_set%22%2C%22cursor%22%3A%22'+data['code']+'%22%2C%22exclude_ids%22%3Anull%2C%22impression_id%22%3A%229d6ea2b2%22%2C%22ref%22%3A%22unknown%22%2C%22experience_type%22%3A%22grammar%22%2C%22story_id%22%3Anull%7D'
 			self.path += '&__user=1078496755&__a=1&__dyn=7AmajEyl2qm9ongDxiWOGeFDzECiq2WiqAdy9VCC-K26m5-bzES2N6y8-bxu3fzoat1bxjx27W88xi5VJ1efKiVWz9EpwzxO2OE&__req=k&__rev=1951399'
 
 		#print self.path
@@ -52,24 +53,33 @@ class FFFExtractor:
 			return -1
 
 	def cursorParser(self,input):
-		position = input.find(CONSTANTS.CURSOR)
-		pageNumberPosition = 0
-		cursorStart = position + 9
+
+		input = input.replace("for (;;);", "", 2)
 		cursor = {'code':'','page_number':''}
 
-		print input
+		try:
+			json_object = json.loads(input)
+		except ValueError, e:
+			position = input.find(CONSTANTS.CURSOR)
+			pageNumberPosition = 0
+			cursorStart = position + 9
 
-		for i in range(cursorStart,len(input)):
-			if input[i] == '"':
-				pageNumberPosition = i + 16
-				for i in range(pageNumberPosition,len(input)):
-					if input[i] == ',':
-						break
-					else:
-						cursor['page_number'] += input[i]
-				break
-			else:
-				cursor['code'] += input[i]
+			for i in range(cursorStart,len(input)):
+				if input[i] == '"':
+					pageNumberPosition = i + 16
+					for i in range(pageNumberPosition,len(input)):
+						if input[i] == ',':
+							break
+						else:
+							cursor['page_number'] += input[i]
+					break
+				else:
+					cursor['code'] += input[i]
+
+		else:
+			pagination = json_object['jsmods']['require'][7][3]
+			cursor['code'] = pagination[0]['cursor']
+			cursor['page_number'] = str(pagination[0]['page_number'])
 
 		print cursor
 		return cursor
@@ -79,7 +89,7 @@ class FFFExtractor:
 
 	def getFans(self):
 		data = None
-		for i in range(1,3):
+		for i in range(1,6):
 			response = self.makeRequest(data)
 			data = self.cursorParser(response)
 
